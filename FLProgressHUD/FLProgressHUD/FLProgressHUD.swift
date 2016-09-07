ec317f4c94d6f90e5f80f39d625dc00ff39369f4
孔凡列 <kongfanlie@clarence-2.local>
2016-09-07 22:27:05 +0800

    init

---
+++ FLProgressHUD/FLProgressHUD/FLProgressHUD.swift
@@ -0,0 +1,27 @@
+//
+//  FLProgressHUD.swift
+//  FLProgressHUD
+//
+//  Created by 孔凡列 on 16/9/7.
+//  Copyright © 2016年 clarence. All rights reserved.
+//
+
+import UIKit
+
+class FLProgressHUD: UIView {
+    static func fl_showHUDAddTo(view : UIView ,withAnimation animation : Bool) -> Self {
+        
+    }
+    
+    func initWith(view view : UIView) {
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ FLProgressHUD/FLProgressHUD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FLProgressHUD
+//
+//  Created by 孔凡列 on 16/9/7.
+//  Copyright © 2016年 clarence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        <#code#>
+    }
+
+}
+
